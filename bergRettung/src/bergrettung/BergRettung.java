c77a3fd7c076e850e87cbc69f2b405ade85506ce
maxbuc <maximilian_bu@t-online.de>
2020-02-17 09:29:47 +0100

    test

--- bergRettung/src/bergrettung/BergRettung.java
+++ bergRettung/src/bergrettung/BergRettung.java
@@ -15,7 +15,7 @@ public class BergRettung {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("ich heiße Max");
     }
     
 }
