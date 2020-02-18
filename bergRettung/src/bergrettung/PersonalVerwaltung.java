c79098270bf84aa32d752c15e3b0b3dc132417a9
maxbuc <maximilian_bu@t-online.de>
2020-02-18 07:42:00 +0100

    personal fertig

--- bergRettung/src/bergrettung/PersonalVerwaltung.java
+++ bergRettung/src/bergrettung/PersonalVerwaltung.java
@@ -1,5 +1,23 @@
 package bergrettung;
 
+import java.util.List;
+
 public class PersonalVerwaltung {
+    PersonalVerwaltungWrapper wrapper = new PersonalVerwaltungWrapper();
     
+    public void insert(Personal p){
+        wrapper.insert(p);
+    }
+    public void delete(int id){
+        wrapper.delete(id);
+    }
+    public void update(Personal p){
+        wrapper.update(p);
+    }
+    public Personal read(int id){
+        return wrapper.read(id);
+    }
+    public List<Personal> readAll(){
+        return wrapper.readAll();
+    }
 }
--- bergRettung/src/bergrettung/PersonalVerwaltungWrapper.java
+++ bergRettung/src/bergrettung/PersonalVerwaltungWrapper.java
@@ -2,7 +2,24 @@ package bergrettung;
 
 import Persistenz.EinfacheFactory;
 import Persistenz.IPersonalMapper;
+import java.util.List;
 
 public class PersonalVerwaltungWrapper {
     IPersonalMapper mapper = EinfacheFactory.createPersonalMapper();
+    
+    public void insert (Personal p){
+        mapper.insert(p);
+    }
+    public void delete (int id){
+        mapper.delete(id);
+    }
+    public void update(Personal p){
+        mapper.update(p);
+    }
+    public Personal read(int id){
+        return mapper.read(id);
+    }
+    public List<Personal> readAll(){
+        return mapper.readAll();
+    }
 }
