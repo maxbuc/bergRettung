371b69bc9affa1e33e085310007c83584398d47a
maxbuc <maximilian_bu@t-online.de>
2020-02-19 10:43:32 +0100

    m

--- bergRettung/src/bergrettung/BergRettung.java
+++ bergRettung/src/bergrettung/BergRettung.java
@@ -16,7 +16,6 @@ public class BergRettung {
         
         setup(persVerw,patVerw, einVerw, equVerw);
         
-        System.out.println(einVerw.readEinsatz(1).toString());
         
         
         MainView mainView = new MainView("Programm - Bergrettung", einVerw , equVerw, patVerw, persVerw);
