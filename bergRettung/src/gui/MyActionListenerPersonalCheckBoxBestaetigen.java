85b57b883aee5829f4672c0ebe21f1c008621da3
maxbuc <maximilian_bu@t-online.de>
2020-02-20 08:51:30 +0100

    checkbox lauft

--- bergRettung/src/gui/MyActionListenerPersonalCheckBoxBestaetigen.java
+++ bergRettung/src/gui/MyActionListenerPersonalCheckBoxBestaetigen.java
@@ -31,6 +31,8 @@ public class MyActionListenerPersonalCheckBoxBestaetigen implements ActionListen
             }
         }
         einView.setPersonalList(personal);
+        
+        
     }
 
 }
